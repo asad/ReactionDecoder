70f06767efdec7b08bb577868f08c60ac1927dd3
Syed Asad Rahman <s9asad@gmail.com>
2016-01-02 14:31:45 +0000

    Reference

--- src/aamtool/Helper.java
+++ src/aamtool/Helper.java
@@ -69,7 +69,7 @@ class Helper extends ChemicalFormatParser {
         sb.append(NEW_LINE);
         sb.append(NEW_LINE);
 
-        sb.append("Reference: Rahman, S.A. et.al.(2015) Reaction Decoder Tool (RTD):");
+        sb.append("Reference: Rahman, S.A. et.al.(2016) Reaction Decoder Tool (RTD):");
         sb.append(NEW_LINE);
         sb.append("Extracting Features from Chemical Reactions (submitted)");
         sb.append(NEW_LINE);
