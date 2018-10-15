e6ea211012bf478a8892b90325949477a659af1a
S. Asad Rahman <s9asad@gmail.com>
2018-10-15 14:54:30 +0100

    Atom Atom Mapping using Java API

--- src/test/java/uk/ac/bioinception/AAMExample.java
+++ src/test/java/uk/ac/bioinception/AAMExample.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2018. BioInception Labs Pvt. Ltd.
  */
 package uk.ac.bioinception;
 
