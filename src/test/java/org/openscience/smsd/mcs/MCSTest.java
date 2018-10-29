8cc34a7b18b39976de6ee1601cde624f1bb2eb04
Syed Asad Rahman <asad@syeds-mbp>
2018-10-29 10:03:32 +0000

    Updated the MCSPlus, speed up!

--- src/test/java/org/openscience/smsd/mcs/MCSTest.java
+++ src/test/java/org/openscience/smsd/mcs/MCSTest.java
@@ -222,10 +222,10 @@ public class MCSTest {
 
 //        IAtomContainer query = sp.parseSmiles("cccc");
 //        IAtomContainer target = sp.parseSmiles("ccnc");
-        IAtomContainer query = sp.parseSmiles("C1=CC=CC=C1");
-        IAtomContainer target = sp.parseSmiles("C1=CC=CC=C1");
-//        IAtomContainer query = sp.parseSmiles("C1OC(CO)C(OP=O)C1O");
-//        IAtomContainer target = sp.parseSmiles("[P](=O)(O)OCC1O[CH]C(OC(=O)C(N)CC2=CC=C(O)C=C2)C1O");
+//        IAtomContainer query = sp.parseSmiles("C1=CC=CC=C1");
+//        IAtomContainer target = sp.parseSmiles("C1=CC=CC=C1");
+        IAtomContainer query = sp.parseSmiles("C1OC(CO)C(OP=O)C1O");
+        IAtomContainer target = sp.parseSmiles("[P](=O)(O)OCC1O[CH]C(OC(=O)C(N)CC2=CC=C(O)C=C2)C1O");
 //        IAtomContainer query = sp.parseSmiles("CC1=C(CCC([O-])=O)C2=[N+]3C1=Cc1c(C)c(C=C)c4C=C5C(C)=C(C=C)C6=[N+]5[Fe-]3(n14)n1c(=C6)c(C)c(CCC([O-])=O)c1=C2");
 //        IAtomContainer target = sp.parseSmiles("C1=2N3C(C=C4[N+]5=C(C=C6N7C8=CC9=[N+](C(=C1)C(=C9CCC([O-])=O)C)[Fe-2]573OC(CCC=%10C=%11C=C%12C(=C(C%13=CC%14=[N+]%15C(=CC=%16N%17C(=C(C%16C=C)C)C=C([N+]%11[Fe-2]%15%17(N%13%12)OC(CCC8=C6C)=O)C%10C)C(=C%14C=C)C)C)CCC([O-])=O)=O)C(=C4C)C=C)=C(C2C)C=C");
         MoleculeInitializer.initializeMolecule(query);
