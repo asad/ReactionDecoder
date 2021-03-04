e20139d6c4b803c3c17a073e2e7894ebd60c5142
Asad Rahman <s9asad@gmail.com>
2021-03-04 21:22:12 +0000

    Test case added

--- src/test/java/uk/ac/ebi/generic/UnblancedReactionChecker.java
+++ src/test/java/uk/ac/ebi/generic/UnblancedReactionChecker.java
@@ -36,14 +36,14 @@ import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IReaction;
 import static org.openscience.cdk.smiles.SmilesGenerator.generic;
 import uk.ac.ebi.reactionblast.mechanism.ReactionMechanismTool;
-import uk.ac.ebi.reactionblast.tools.TestUtility;
+import uk.ac.ebi.reactionblast.tools.MappingUtility;
 import uk.ac.ebi.reactionblast.tools.rxnfile.MDLRXNV2000Reader;
 
 /**
  * @contact Syed Asad Rahman, EMBL-EBI, Cambridge, UK.
  * @author Syed Asad Rahman <asad @ ebi.ac.uk>
  */
-public class UnblancedReactionChecker extends TestUtility {
+public class UnblancedReactionChecker extends MappingUtility {
 
     private static final boolean DEBUG = false;
     private static final File DIR = new File(RHEA_RXN_DIR);
